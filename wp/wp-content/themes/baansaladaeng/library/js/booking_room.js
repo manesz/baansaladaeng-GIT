596597e8ed49e820a9fdc0e80f9c9bc942277d30
ruxchuk <ruxchuk@gmail.com>
2014-11-29 01:06:08 +0700

    edit booking

--- wp/wp-content/themes/baansaladaeng/library/js/booking_room.js
+++ wp/wp-content/themes/baansaladaeng/library/js/booking_room.js
@@ -21,31 +21,49 @@ $jConflict(document).ready(function () {
 //                    today: 'Today',
 //                    month: 'Month'
 //                },
-            editable: false,
+//            editable: false,
             events: obj_event,
             selectable: true,
             //                selectHelper: true,
+            /*dayClick: function (date, jsEvent, view) {
+
+             //                alert('Clicked on: ' + date);
+             //
+             //                alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+             //
+             //                alert('Current view: ' + console.log(view));
+             //                console.log(view)
+             var strDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+             //                if (!removeBookingDate(strDate)) {
+             //                    addBookingDateToArray(strDate);
+             //                }
+             // change the day's background color just for fun
+             var bg = $(this).css('background-color');
+             if (bg == 'rgb(30, 236, 255)')
+             $(this).css('background-color', '#ffffff');
+             else //if (bg == "rgba(0, 0, 0, 0)")
+             $(this).css('background-color', '#1EECFF');
+
+             //                var hasClass = $(this).hasClass('bg-select');
+             //                if (hasClass) {
+             //                    $(this).addClass('bg-no-select');
+             //                    $(this).removeClass('bg-select');
+             //                    removeBookingDate(strDate);
+             //                } else {
+             //                    checkDateRoom(strDate, strDate, this);
+             //                }
+
+             },*/
             select: function (start, end, allDay, resourceId) {
 //                    var title = prompt('Event Title:');
 //                    if (title) {
                 if (!check_add_event) {
-                    $jConflict('#calendar').fullCalendar('renderEvent',
-                        {
-                            title: '',
-                            start: start,
-                            end: end,
-                            allDay: allDay,
-                            backgroundColor: '#00A2E8',
-                            resourceId: resourceId
-                        },
-                        true // make the event "stick"
-                    );
-                    var strDateCheckIn = start.getDate() + "/" + (start.getMonth() + 1) + "/" + start.getFullYear();
-                    var strDateCheckOut = end.getDate() + "/" + (end.getMonth() + 1) + "/" + end.getFullYear();
-                    $jConflict("#check_in_date").val(strDateCheckIn);
-                    $jConflict("#check_out_date").val(strDateCheckOut);
+//                    $jConflict("#check_in_date").val(strDateCheckIn);
+//                    $jConflict("#check_out_date").val(strDateCheckOut);
+//                    addBookingDateToArray(strDateCheckIn, strDateCheckOut);
+                    checkDateRoom(start, end, allDay, resourceId);
                 }
-                check_add_event = true;
+//                check_add_event = true;
                 /**
                  * ajax call to store event in DB
                  */
@@ -72,11 +90,22 @@ $jConflict(document).ready(function () {
                 //                            , { "id": calEvent.id }
                 //                        );
                 // delete in frontend
+//                alert(calEvent._id)
                 if (calEvent.title != "X") {
                     $jConflict("#calendar").fullCalendar('removeEvents', calEvent._id);
                     check_add_event = false;
                     $jConflict("#check_in_date").val("");
                     $jConflict("#check_out_date").val("");
+
+                    var strDateCheckIn = calEvent.start.getFullYear() + "-" + (calEvent.start.getMonth() + 1) + "-" + calEvent.start.getDate();
+                    var strDateCheckOut = strDateCheckIn;
+                    if (calEvent.end) {
+                        strDateCheckOut = calEvent.end.getFullYear() + "-" + (calEvent.end.getMonth() + 1) + "-" + calEvent.end.getDate();
+                    }
+
+                    removeBookingDate(strDateCheckIn, strDateCheckOut);
+                } else {
+                    return false;
                 }
 
                 //                        }
@@ -86,40 +115,117 @@ $jConflict(document).ready(function () {
 });
 
 var array_booking_date = [];
+var array_booking_date_group = [];
 
-function addBookingDate(strDate) {
-    array_booking_date.push(strDate);
+function addBookingDateToArray(checkIn, checkOut) {
+    array_booking_date.push(checkIn + "|" + checkOut);
+    array_booking_date.sort();
+    //sortBookingDate();
 }
 
-function removeBookingDate(strDate) {
+function sortBookingDate() {
+    array_booking_date.sort();
+    array_booking_date_group = [];
+    var oldDate = "";
+    var checkIn = "";
+    for (var i = 0; i < array_booking_date.length; i++) {
+        var getDate = array_booking_date[i];
+        if (i == 0) {
+            checkIn = getDate + "|";
+        } else {
+            var dateCheck1 = new Date(getDate);
+            var dateCheck2 = new Date(oldDate);
+            var diff = Math.abs(dateCheck2 - dateCheck1);
+            checkIn = diff;
+        }
+        oldDate = getDate;
+    }
+    if (array_booking_date.length > 0)
+        array_booking_date_group.push(checkIn + oldDate);
+    alert(array_booking_date_group);
+}
+
+function removeBookingDate(checkIn, checkOut) {
+    var strDate = checkIn + "|" + checkOut;
     var newArrayBookingDate = [];
+    var checkRemove = false;
     for (var i = 0; i < array_booking_date.length; i++) {
         var strOldDate = array_booking_date[i];
         if (strOldDate != strDate) {
             newArrayBookingDate.push(strOldDate);
-        }
+        } else checkRemove = true;
     }
     array_booking_date = newArrayBookingDate;
+    return checkRemove;
 }
 
 $jConflict(document).on("submit", "#form_room_submit", function (e) {
-    if (this.check_in_date.value == "" && this.check_in_date.value == "") {
-        return true;
-    } else if (!check_post_data) {
-        checkDateRoom(this);
-        return false;
-    } else {
-        return true;
-    }
+    this.array_date.value = array_booking_date;
 });
+/*
+ function checkDateRoom(elm) {
+ var data = {
+ booking_post: 'true',
+ reservation_post: 'check_room',
+ room_id: elm.room_id.value,
+ check_in: elm.check_in_date.value,
+ check_out: elm.check_out_date.value
+ };
+ $jConflict.ajax({
+ type: "POST",
+ url: '',
+ data: data,
+ success: function (data) {
+ if (data != 'yes') {
+ check_post_data = false;
+ alert(data);
+ } else {
+ check_post_data = true;
+ $jConflict("#form_room_submit").submit();
+ }
+ },
+ error: function (result) {
+ alert("Error:\n" + result.responseText);
+ }
+ });
+ }*/
 
-function checkDateRoom(elm) {
+function checkDateInArray(start, end) {
+
+    var check1 = new Date(start);
+    var check2 = new Date(end);
+    var dateNow = new Date(date_now);
+    if (check1 < dateNow)
+        return false;
+    for (var i = 0; i < array_booking_date.length; i++) {
+        var dateSp = array_booking_date[i].split('|');
+        var from = new Date(dateSp[0]);  // -1 because months are from 0 to 11
+        var to = new Date(dateSp[1]);
+
+        if (check1 >= from && check1 <= to || check2 >= from && check2 <= to) {
+            return false;
+        }
+        if (check1 < from && check1 < to && check2 > from && check2 > to) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function checkDateRoom(start, end, allDay, resourceId) {
+    var strDateCheckIn = start.getFullYear() + "-" + (start.getMonth() + 1) + "-" + start.getDate();
+    var strDateCheckOut = end.getFullYear() + "-" + (end.getMonth() + 1) + "-" + end.getDate();
+    if (!checkDateInArray(strDateCheckIn, strDateCheckOut)) {
+        //alert("Please check your date.");
+        return;
+    }
     var data = {
         booking_post: 'true',
         reservation_post: 'check_room',
-        room_id: elm.room_id.value,
-        check_in: elm.check_in_date.value,
-        check_out: elm.check_out_date.value
+        room_id: room_id,
+        check_in: strDateCheckIn,
+        check_out: strDateCheckOut
     };
     $jConflict.ajax({
         type: "POST",
@@ -127,11 +233,46 @@ function checkDateRoom(elm) {
         data: data,
         success: function (data) {
             if (data != 'yes') {
-                check_post_data = false;
+                //check_post_data = false;
                 alert(data);
             } else {
-                check_post_data = true;
-                $jConflict("#form_room_submit").submit();
+                addBookingDateToArray(strDateCheckIn, strDateCheckOut);
+                $jConflict('#calendar').fullCalendar('renderEvent',
+                    {
+                        title: '*',
+                        start: start,
+                        end: end,
+                        allDay: allDay,
+                        backgroundColor: '#00A2E8',
+                        resourceId: resourceId
+                    },
+                    true // make the event "stick"
+                );
+            }
+        },
+        error: function (result) {
+            alert("Error:\n" + result.responseText);
+        }
+    });
+}
+
+function postAddBooking() {
+    $jConflict.ajax({
+        type: "POST",
+        url: '',
+        data: {
+            booking_post: 'true',
+            reservation_post: 'add_array_booking',
+            room_id: room_id,
+            room_name: $("#room_name").val(),
+            array_booking: array_booking_date
+        },
+        success: function (data) {
+            if (data != 'success') {
+                //check_post_data = false;
+                alert(data);
+            } else {
+                window.location.href = webUrl + 'reservation?payment=true'
             }
         },
         error: function (result) {
